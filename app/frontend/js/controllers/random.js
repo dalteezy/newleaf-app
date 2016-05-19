3241d773033985534ad0da28c9e580a9e1ab372f
Dalton <dalteezy@yahoo.com>
2016-05-18 18:22:44 -0700

    toggle menu on click using ng-show/ng-hide

--- app/frontend/js/controllers/random.js
+++ app/frontend/js/controllers/random.js
@@ -3,4 +3,12 @@
 angular.module('newLeafEfficiency')
   .controller('RandomCtrl', function ($rootScope) {
 	$rootScope.pageTitle = 'Random';
+ })
+  .controller('menuHideCtrl', function ($rootScope) {
+  	$rootScope.helloWorld = function () {
+  		console.log('This is working!')
+  	};
+  	$rootScope.showMenu = function () {
+  		
+  	};
  });
