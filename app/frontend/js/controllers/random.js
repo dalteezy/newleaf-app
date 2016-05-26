7dcae52e953ca67759a1a44736fb3690fffb82e5
Joshua Warejko <josh@newleafinnovations.com>
2016-05-25 18:07:57 -0700

    Changed some structure and added efficiency calculator.

--- app/frontend/js/controllers/random.js
+++ app/frontend/js/controllers/random.js
@@ -1,14 +1,25 @@
 'use strict';
 
 angular.module('newLeafEfficiency')
-  .controller('RandomCtrl', function ($rootScope) {
+.controller('RandomCtrl', function ($rootScope) {
 	$rootScope.pageTitle = 'Random';
- })
-  .controller('menuHideCtrl', function ($rootScope) {
-  	$rootScope.helloWorld = function () {
-  		console.log('This is working!')
-  	};
-  	$rootScope.showMenu = function () {
-  		
-  	};
- });
+})
+.controller('menuHideCtrl', function ($rootScope) {
+	$rootScope.helloWorld = function () {
+		console.log('This is working!')
+	};
+	$rootScope.showMenu = function () {
+
+	}
+})
+.controller('calculator', ['$scope', function($scope) {
+	$scope.calculation = 0;
+  $scope.number1 = '';
+  $scope.number2 = '';
+  $scope.submit = function() {
+  	var random = parseInt($scope.number1) / parseInt($scope.number2);
+  	$scope.calculation = random.toPrecision(4);
+  };
+}]);
+
+
