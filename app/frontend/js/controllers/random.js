f313c24331edc9bc685847a9000b8452b7ce4ede
Joshua Warejko <josh@newleafinnovations.com>
2016-05-31 08:07:48 -0700

    Pulling time logs for Josh

--- app/frontend/js/controllers/random.js
+++ app/frontend/js/controllers/random.js
@@ -32,12 +32,13 @@ angular.module('newLeafEfficiency')
         then(function(response) {   // "then" basically means that the connection was successful, so move on.
           $scope.status = response.status;
           $scope.data = response.data;
+          console.log("practiceGet api status: " + response.status);
         }, function(response) {
           $scope.data = response.data || "Request failed";
           $scope.status = response.status;
       });
 }])
-.controller('teamworkGet', ['$scope', '$http', '$templateCache',
+.controller('teamworkGetPeople', ['$scope', '$http', '$templateCache',
   function($scope, $http, $templateCache) {
     $scope.method = 'GET';
     $scope.token = 'mouth726brown';
@@ -55,6 +56,29 @@ angular.module('newLeafEfficiency')
               $scope.employees.push(person);
             }
           });
+          console.log("teamworkGetPeople api status: " + response.status);
+        }, function(response) {
+          $scope.data = response.data || "Request failed";
+          $scope.status = response.status;
+      });
+}])
+.controller('teamworkGetTime', ['$scope', '$http', '$templateCache',
+  function($scope, $http, $templateCache) {
+    $scope.method = 'GET';
+    $scope.token = 'mouth726brown';
+    $scope.url = 'https://projects.newleaf.team/projects/183376/time_entries.json?userId=57450&fromdate=20160527&todate=20160527';
+
+      $scope.response = null;
+      $scope.joshEntries = 0;
+
+      $http({method: $scope.method, url: $scope.url, cache: $templateCache,  headers: {'Content-Type': 'application/json; text/javascript; */*', 'Authorization': 'BASIC ' + window.btoa($scope.token + ":xxx")}}) // $http has many more options for parameters.
+        .then(function(response) {   // "then" basically means that the connection was successful, so move on.
+          $scope.status = response.status;
+          $scope.data = response.data;
+          angular.forEach(response.data["time-entries"], function(time) {
+            $scope.joshEntries += parseInt(time.minutes) + parseInt(time.hours)*60;
+          });
+          console.log("teamworkGetTime api status: " + response.status);
         }, function(response) {
           $scope.data = response.data || "Request failed";
           $scope.status = response.status;
