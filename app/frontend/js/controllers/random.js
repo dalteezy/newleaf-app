f87fe9fc2a1b84b512be4bfb8521339968967529
Joshua Warejko <josh@newleafinnovations.com>
2016-05-31 17:51:04 -0700

    Added get current date to controller

--- app/frontend/js/controllers/random.js
+++ app/frontend/js/controllers/random.js
@@ -26,7 +26,7 @@ angular.module('newLeafEfficiency')
     $scope.method = 'GET';
     $scope.url = 'http://ipinfo.io/json';
 
-      $scope.response = null;
+    $scope.response = null;
 
       $http({method: $scope.method, url: $scope.url, cache: $templateCache}). // $http has many more options for parameters.
         then(function(response) {   // "then" basically means that the connection was successful, so move on.
@@ -36,53 +36,58 @@ angular.module('newLeafEfficiency')
         }, function(response) {
           $scope.data = response.data || "Request failed";
           $scope.status = response.status;
-      });
-}])
+        });
+      }])
 .controller('teamworkGetPeople', ['$scope', '$http', '$templateCache',
   function($scope, $http, $templateCache) {
     $scope.method = 'GET';
     $scope.token = 'mouth726brown';
     $scope.url = 'https://projects.newleaf.team/people.json/';
 
-      $scope.response = null;
-      $scope.employees = [];
+    $scope.response = null;
+    $scope.employees = [];
 
-      $http({method: $scope.method, url: $scope.url, cache: $templateCache,  headers: {'Content-Type': 'application/json; text/javascript; */*', 'Authorization': 'BASIC ' + window.btoa($scope.token + ":xxx")}}) // $http has many more options for parameters.
-        .then(function(response) {   // "then" basically means that the connection was successful, so move on.
-          $scope.status = response.status;
-          $scope.data = response.data;
-          angular.forEach(response.data.people, function(person) {
-            if(person["company-name"] === "NewLeaf") {
-              $scope.employees.push(person);
-            }
-          });
-          console.log("teamworkGetPeople api status: " + response.status);
-        }, function(response) {
-          $scope.data = response.data || "Request failed";
-          $scope.status = response.status;
+    $http({method: $scope.method, url: $scope.url, cache: $templateCache,  headers: {'Content-Type': 'application/json; text/javascript; */*', 'Authorization': 'BASIC ' + window.btoa($scope.token + ":xxx")}}) // $http has many more options for parameters.
+      .then(function(response) {   // "then" basically means that the connection was successful, so move on.
+        $scope.status = response.status;
+        $scope.data = response.data;
+        console.log("teamworkGetPeople api status: " + response.status);
+        angular.forEach(response.data.people, function(person) {
+          if(person["company-name"] === "NewLeaf") {
+            $scope.employees.push(person);
+          }
+        });
+      }, function(response) {
+        $scope.data = response.data || "Request failed";
+        $scope.status = response.status;
       });
 }])
 .controller('teamworkGetTime', ['$scope', '$http', '$templateCache',
   function($scope, $http, $templateCache) {
+    var date = new Date();
+    $scope.todayDate = date.getFullYear() + ('0' + (date.getMonth() + 1)).slice(-2) + ('0' + date.getDate()).slice(-2);
     $scope.method = 'GET';
     $scope.token = 'mouth726brown';
-    $scope.url = 'https://projects.newleaf.team/projects/183376/time_entries.json?userId=57450&fromdate=20160527&todate=20160527';
 
-      $scope.response = null;
-      $scope.joshEntries = 0;
+    
+    
+    $scope.url = 'https://projects.newleaf.team/projects/183376/time_entries.json?userId=57450&fromdate=20160527&todate=' + $scope.todayDate;
 
+    $scope.response = null;
+    $scope.joshEntries = 0;
+    console.log($scope.FromDate);
       $http({method: $scope.method, url: $scope.url, cache: $templateCache,  headers: {'Content-Type': 'application/json; text/javascript; */*', 'Authorization': 'BASIC ' + window.btoa($scope.token + ":xxx")}}) // $http has many more options for parameters.
         .then(function(response) {   // "then" basically means that the connection was successful, so move on.
           $scope.status = response.status;
           $scope.data = response.data;
+          console.log("teamworkGetTime api status: " + response.status);
           angular.forEach(response.data["time-entries"], function(time) {
             $scope.joshEntries += parseInt(time.minutes) + parseInt(time.hours)*60;
           });
-          console.log("teamworkGetTime api status: " + response.status);
         }, function(response) {
           $scope.data = response.data || "Request failed";
           $scope.status = response.status;
-      });
+        });
 }])
 
 
